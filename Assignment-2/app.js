582ad7bd15c1dc31dbb534b2029598cc3d29bf7e
Kristopher Williams <kristopher.williams2@gmail.com>
2020-04-27 14:27:42 -0500

    router implemented

--- Assignment-2/app.js
+++ Assignment-2/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use("/users", (req, res, next) => {
+app.use('/users', (req, res, next) => {
     console.log("Users page works!");
     res.send("<h1>This is the userspage</h1>");
 });
 
 app.use('/', (req, res, next) => {
-  console.log('Homepage works!');
-    res.send('<h1>This is the homepage</h1>');
+  console.log("Homepage works!");
+    res.send("<h1>This is the homepage</h1>");
 });
 
 app.listen(3000);
--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-    console.log("This always runs!");
-    next();
-});
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-    res.send("<h1>The add product page!</h1>");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-    console.log("In the middleware!");
-    res.send("<h1>Hello from express!</h1>");
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
