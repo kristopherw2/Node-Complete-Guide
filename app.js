9d6d78b31ef4f670a8b39d6a6dbbd323184efcf4
Kristopher Williams <kristopher.williams2@gmail.com>
2020-04-30 16:33:43 -0500

    added styling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -7,12 +9,15 @@ const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public',)))
 
 app.use("/admin", adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>");
+    res.status(404).sendFile(
+        path.join(__dirname, "views", "page-not-found.html")
+    );
 });
 
 app.listen(3000);
